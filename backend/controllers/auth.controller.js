44e67c50187ecc319b758d0c25fa1eca34b48a1c
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-09-21 09:00:30 +0530

    controllers,models,routes are added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req,res)=>{
+    res.send("This is SignUP route")
+}
+
+export const signin = async (req,res)=>{
+    res.send("This is SignIN route")
+}
+
+export const signout = async (req,res)=>{
+    res.send("This is SignOUT route")
+}
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js"
+
 
 dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.get("/",(req,res)=>{
     res.send("Hello There this is my first connection with mongoDB")
 });
 
-app.listen(3000,()=>{
+app.use("/api/auth", authRoutes);
+
+app.listen(PROT,()=>{
     connectDB();
-    console.log('Server is running');
+    console.log(`Server is running : http://localhost:PORT/`);
     
 });
 
